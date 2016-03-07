78dc30e8062cc69b083677b468ec9a1c1f27f9fb
bdepwgjqet <bdepwgjqet@gmail.com>
2016-03-08 01:33:27 +0800

    sync

--- cogen/cmdline.py
+++ cogen/cmdline.py
@@ -3,3 +3,13 @@
 #
 # @author bdepwgjqet[at]gmail.com
 
+import sys
+
+
+def execute(argv=None):
+    if argv is None:
+        argv = sys.argv
+    print 'go'
+
+if __name__ == '__main__':
+    execute()
