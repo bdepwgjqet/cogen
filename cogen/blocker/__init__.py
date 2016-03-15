8b5bab8548cc844d403b6152ff00afdaf2f03923
bdepwgjqet <bdepwgjqet@gmail.com>
2016-03-15 15:54:36 +0800

    sync

---
+++ cogen/blocker/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Block(object):
+
+    def __init__(self, lang):
+        self.lang = lang
+        self.code = []
--- cogen/settings.py
+++ cogen/settings.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# @author bdepwgjqet[at]gmail.com
 
+
+lamark = {
+    '```python': 'python'
+}
